3aefb37da4e1965904ccf7505ceae41eb10b4e24
ArquimedesGarcia <arquimedesallan02@gmail.com>
2025-07-17 23:55:45 -0300

    Add question answer with Gemini AI

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { fastifyCors } from "@fastify/cors"
+import { fastifyMultipart } from "@fastify/multipart"
 import { fastify } from "fastify"
 import {
 	serializerCompiler,
@@ -10,8 +11,12 @@ import { getRoomsRoute } from "./http/routes/get-rooms.ts"
 import { createRoomRoute } from "./http/routes/create-room.ts"
 import { getRoomQuestions } from "./http/routes/get-room-questions.ts"
 import { createQuestionRoute } from "./http/routes/create-question.ts"
+import { uploadAudioRoute } from "./http/routes/upload-audio.ts"
+
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyMultipart)
 
 app.register(fastifyCors, {
 	origin: "http://localhost:5173",
@@ -21,6 +26,7 @@ app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomQuestions)
 app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
